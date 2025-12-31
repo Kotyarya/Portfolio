79028b5a18c73994cae76ff55488fdbbe9255106
Kotyarya <sunguuner500@gmail.com>
2025-12-31 03:53:34 +0100

    refactoring

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,19 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http',
-                hostname: 'portfolio-server-0e3k.onrender.com',
-                port: '10000',
-                pathname: '/media/**',
+                protocol: "https",
+                hostname: "portfolio-server-0e3k.onrender.com",
+                pathname: "/media/**",
             },
+            // локалка (если реально используешь в dev)
             {
-                protocol: 'http',
-                hostname: '192.168.100.62',
-                port: '4000',
-                pathname: '/media/**',
-            }
-        ]
+                protocol: "http",
+                hostname: "192.168.100.62",
+                port: "4000",
+                pathname: "/media/**",
+            },
+        ],
     },
-}
+};
 
 export default nextConfig;
